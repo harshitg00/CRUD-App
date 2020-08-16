12bd157cd82d20c4dc7f64dca2ba2d5917bd1d97
Harshit Gupta <lcs2019008@iiitl.ac.in>
2020-08-16 13:38:09 +0530

    Hopefully

--- models/db.js
+++ models/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const keys = null;
 // const keys = require("../config/keys");
-mongoose.connect(keys.MongoURI || process.env.mongoURI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true },
+mongoose.connect(process.env.mongoURI , { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true },
   (err) => {
     if (err) {
       console.log(err);
